0b8d379acca43fdad91b05f38c88ed9061c0d887
dexpie <gadingputra103@gmail.com>
2026-06-08 09:09:59 +0700

    pertemuan 14

--- resources/views/template2.blade.php
+++ resources/views/template2.blade.php
@@ -27,7 +27,7 @@
                         <a class="nav-link" href="/siswa">Siswa</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/meja">Meja</a>
+                        <a class="nav-link" href="#">PR 1</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">PR 2</a>
