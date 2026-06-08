a7624d7632f68f59931c4b1d032203edb271aaac
dexpie <gadingputra103@gmail.com>
2026-06-08 08:18:00 +0700

    pertemuan 14

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -2,51 +2,28 @@
 <html lang="en">
 
 <head>
-    <title>5026241129 Gading Putra Priyanto</title>
+    <title>Latihan Responsive Web</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-
     <div class="container">
-        <div class="mt-4 p-5 bg-primary text-white rounded">
-            <h3>5026241129 Gading Putra Priyanto</h3>
-            <h6>@yield('title')</h6>
-        </div>
-        <nav class="navbar navbar-expand-sm bg-light navbar-light">
-            <div class="container-fluid">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/pegawai">Pegawai</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/siswa">Siswa</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/meja">Meja</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">PR 2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">PR 3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">EAS</a>
-                    </li>
+        <div class="jumbotron text-center">
+            <h1 class="display-3">Selamat Datang</h1>
+            <p class="lead">di Pemrograman Web</p>
+            <hr class="my-2">
+            <p>More info</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Klik Disini</a>
 
-                </ul>
-            </div>
-        </nav>
-        <div class="container">
-            @yield('konten')
+            </p>
         </div>
     </div>
-
 </body>
 
 </html>
